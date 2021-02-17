9dcdee9cec5b5cc01f2bc7dfae10a54796cea4c9
Anthony Correia <anthony.correia@epfl.ch>
2021-02-17 03:51:12 +0100

    small mistake...

--- library/HEA/__init__.py
+++ library/HEA/__init__.py
@@ -4,7 +4,7 @@ HEA (High Energy Analysis) is a personal package to ease analyses of LHCb data:
 * Load root files with :py:mod:`HEA.pandas_root`
 * Produce plots with : :py:mod:`HEA.plot`
 * Perform fits with :py:mod:`HEA.fit`
-    * Train a Boosted Decision Tree with :py:mod:`HEA.BDT`
+* Train a Boosted Decision Tree with :py:mod:`HEA.BDT`
 * Some tools are also available in :py:mod:`HEA.tools`. They are rather used within this package.
 """
 
